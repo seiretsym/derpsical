70b56445a9a7187b2475c8741df5d9f805e195f9
Kerwin Hy <kerwin.hy@gmail.com>
2019-11-08 15:59:01 -0800

    deploy #9

--- server.js
+++ server.js
@@ -9,16 +9,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// routes
-app.use(routes);
-
 // for heroku
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-} else {
-    // everything else
-    app.use(express.static("client/public"));
 }
+
+// routes
+app.use(routes);
+
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // connect to mongo~
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/derpsical");
