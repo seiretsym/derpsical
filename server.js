2854869a91400f00cf85e4105729597c138c0bb1
Kerwin Hy <kerwin.hy@gmail.com>
2019-11-08 15:50:38 -0800

    deploy #7

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require("./routes");
 
 // server config
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
