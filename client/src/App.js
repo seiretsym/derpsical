6ae3a0cbbde17bb2159840f5d154f074ad2a8aad
Kerwin Hy <kerwin.hy@gmail.com>
2019-11-08 17:11:19 -0800

    deploy #22

--- client/src/App.js
+++ client/src/App.js
@@ -93,13 +93,13 @@ class App extends Component {
           <Header />
           <Nav login={this.state.login} user={this.state.profile.displayname} handleLogin={() => this.setModalShow(true)} handleLogout={this.handleLogout} />
           <Switch>
-            <Route exact path="/" component={Demo} />
-            <Route path="/demo" component={Demo} />
-            <Route path="/profile" render={(props) => <Profile profile={this.state.profile} loggedin={this.state.login} {...props} />} />
-            <Route path="/composer" render={(props) => <Composer profile={this.state.profile} loggedin={this.state.login} {...props} />} />
-            <Route path="/songs/:id" render={(props) => <Player {...props} />} />
-            <Route path="/composer/:id" render={(props) => <Editor {...props} />} />
-            <Route path="/profiles/:id" render={(props) => <User loggedin={this.state.login} uid={this.state.profile._id} {...props} />} />
+            <Route exact path="/" component={Main} />
+            <Route exact path="./demo" component={Demo} />
+            <Route exact path="./profile" render={(props) => <Profile profile={this.state.profile} loggedin={this.state.login} {...props} />} />
+            <Route exact path="./composer" render={(props) => <Composer profile={this.state.profile} loggedin={this.state.login} {...props} />} />
+            <Route exact path="./songs/:id" render={(props) => <Player {...props} />} />
+            <Route exact path="./composer/:id" render={(props) => <Editor {...props} />} />
+            <Route exact path="./profiles/:id" render={(props) => <User loggedin={this.state.login} uid={this.state.profile._id} {...props} />} />
             <Route component={Main} />
           </Switch>
           <LoginModal
