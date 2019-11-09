8a942b0d77342589112f5caf12dfafa2d3f625b4
Kerwin Hy <kerwin.hy@gmail.com>
2019-11-08 18:17:11 -0800

    deploy #37

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Components
 import Header from "./components/Header";
 import Nav from "./components/Nav";
