b67214ef9e0113643b32ff0dc2abb230f1ee3cb5
Kerwin Hy <kerwin.hy@gmail.com>
2019-11-08 17:51:36 -0800

    deploy #31

--- client/src/App.js
+++ client/src/App.js
@@ -88,26 +88,27 @@ class App extends Component {
     };
 
     return (
-      <BrowserRouter>
+      <Router>
         <div className="container">
           <Header />
           <Nav login={this.state.login} user={this.state.profile.displayname} handleLogin={() => this.setModalShow(true)} handleLogout={this.handleLogout} />
           <Switch>
-            <Route exact path="/" component={Main} />
-            <Route exact path="/demo" component={Demo} />
-            <Route exact path="/profile" render={() => <Profile profile={this.state.profile} loggedin={this.state.login} />} />
-            <Route exact path="/composer" render={() => <Composer profile={this.state.profile} loggedin={this.state.login} />} />
-            <Route exact path="/songs/:id" render={(props) => <Player {...props} />} />
-            <Route exact path="/composer/:id" render={(props) => <Editor {...props} />} />
-            <Route exact path="/profiles/:id" render={(props) => <User loggedin={this.state.login} uid={this.state.profile._id} {...props} />} />
-            <Route component={Main} />
+            <Route exact path="/" component={Main}>
+              <Route path="demo" component={Demo} />
+              <Route path="profile" render={() => <Profile profile={this.state.profile} loggedin={this.state.login} />} />
+              <Route path="composer" render={() => <Composer profile={this.state.profile} loggedin={this.state.login} />} />
+              <Route path="songs/:id" render={(props) => <Player {...props} />} />
+              <Route path="composer/:id" render={(props) => <Editor {...props} />} />
+              <Route path="profiles/:id" render={(props) => <User loggedin={this.state.login} uid={this.state.profile._id} {...props} />} />
+              <Route component={Main} />
+            </Route>
           </Switch>
           <LoginModal
             show={this.state.modalShow}
             onHide={() => this.setModalShow(false)}
           />
         </div>
-      </BrowserRouter>
+      </Router>
     );
   };
 };
